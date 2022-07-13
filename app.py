cbd5b4c65a0c352bcaab7eac5c3fcbb98098adc0
hanbaek-Lee <dlgksqor9105@gmail.com>
2022-07-14 01:18:51 +0900

    detail 로고 클릭시 index 전환 업데이트

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import hashlib
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
-
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -21,6 +22,96 @@ db = client.animal
 
 
 
+@app.route('/hospital', methods=["POST"])
+def information_post():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(
+        'https://www.google.com/localservices/prolist?g2lbs=AL1YbfU2_G6oRy7bHObp5Xqp13bAnvJsRsvZOWdGghzny7VniZ6jfVjHIJCdCZLDu0y0FRWOblja&hl=ko-KR&gl=kr&ssta=1&q=%EA%B2%BD%EA%B8%B0%EB%8F%84%EB%8F%99%EB%AC%BC%EB%B3%91%EC%9B%90&oq=%EA%B2%BD%EA%B8%B0%EB%8F%84%EB%8F%99%EB%AC%BC%EB%B3%91%EC%9B%90&scp=ChRnY2lkOmFuaW1hbF9ob3NwaXRhbBJJEhIJjbZ-5eR5fDURwrX21pgfDAEaEgmbuhHL61VkNRFL24i6AJskkSIJ6rK96riw64-EKhQNppL9FRW6-ExLHRaG1BYl_p41TBoM64-Z66y867OR7JuQIhXqsr3quLDrj4Trj5nrrLzrs5Hsm5AqDOuPmeusvOuzkeybkA%3D%3D&slp=MgBSAggC&src=2&origin=https%3A%2F%2Fwww.google.com&sa=X&ved=2ahUKEwia6fGHmfL4AhUhqFYBHR-OB6wQjGp6BAgCEEY&lci=0',
+        headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    hospitals = soup.select(
+        '#yDmH0d > c-wiz > div > div:nth-child(3) > div > div > div.XJInM > div.YhtaGd.aQOEkf > div.jq95K > c-wiz > div > div > div.Jtakfe > c-wiz > div > div')
+    for hospital in hospitals:
+        a1 = hospital.select_one('div.deyx8d > div > div > div.YtX3Td > div.rgnuSb.xYjf2e')
+        if (a1 != None):
+            name = a1.text
+
+            b1 = hospital.select_one(
+                'div.deyx8d > div > div > div.YtX3Td > div:nth-child(3) > span:nth-child(1) > span')
+            if (b1 != None):
+                opening_hours = b1.text
+            else:
+                opening_hours = ""
+
+            c1_1 = hospital.select_one('div.deyx8d > div > div > div.YtX3Td > div:nth-child(3) > span:nth-child(1)')
+            c1_2 = hospital.select_one('div.deyx8d > div > div > div.YtX3Td > div:nth-child(3) > span:nth-child(2)')
+            if (c1_1 != None):
+                tel_1 = c1_1.text
+            else:
+                tel_1 = ""
+
+            if (c1_2 != None):
+                tel_2 = c1_2.text
+            else:
+                tel_2 = ""
+
+            if '-' not in tel_1:
+                tel_1 = None
+                if (tel_1 == None):
+                    tel_1 = ""
+            if '-' not in tel_2:
+                tel_2 = None
+                if (tel_2 == None):
+                    tel_2 = ""
+
+            phone = tel_1 + tel_2
+
+            if (phone == ""):
+                phone = "존재하지 않습니다"
+
+            d1_1 = hospital.select_one(
+                'div.deyx8d > div > div > div.YtX3Td > div:nth-child(3) > span:nth-child(2) > span')
+            d1_2 = hospital.select_one(
+                'div.deyx8d > div > div > div.YtX3Td > div:nth-child(3) > span:nth-child(3) > span')
+
+            if (d1_1 != None):
+                add1 = d1_1.text
+            else:
+                add1 = ""
+
+            if (d1_2 != None):
+                add2 = d1_2.text
+            else:
+                add2 = ""
+
+            address = add1 + add2
+
+            if (address == ""):
+                address = "존재하지 않습니다"
+
+            e1 = hospital.select_one('div.deyx8d > div > div > div.z6yOqb.i1baHb > img')
+            if (e1 != None):
+                photo = e1['src']
+            else:
+                photo = 'https://previews.123rf.com/images/siamimages/siamimages1504/siamimages150401064/39173277-%EC%82%AC%EC%A7%84-%EC%97%86%EC%9D%8C-%EC%95%84%EC%9D%B4%EC%BD%98-%EC%97%86%EC%9D%8C.jpg'
+
+            doc = {
+                'photo': photo,
+                'name': name,
+                'phone': phone,
+                'address': address,
+                'opening_hours': opening_hours
+            }
+
+            return jsonify({'msg1': '업로드 완료'})
+
+
+
+
+
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
@@ -32,6 +123,19 @@ def home():
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+
+@app.route("/hospital", methods=["GET"])
+def information_get():
+    hospital_list = list(db.project.find({}, {'_id': False}))
+    return jsonify({'hospital': hospital_list})
+
+
+@app.route('/detail')
+def detail():
+    return render_template("detail.html")
+
 
 
 @app.route('/login')
